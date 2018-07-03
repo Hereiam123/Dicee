5ce815759216c99de6f936b69916d95dc3b53707
user139727 <user139727@74-80-245-36.ic2net.net>
2018-07-02 19:21:35 -0700

    Added image and button connection to code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
 
 }
 
